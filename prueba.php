1e3ae501ebfd772aa8646bb2918aabd33e4323cc
NoemiRA <201821077@tese.edu.mx>
2023-01-20 14:45:22 -0600

    Cambios 20-01-2023

--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-$array = array(1, 2, 3, 4);
-
-foreach ($array as $a) {
-    // $a contiene el primer elemento del array interior,
-    // y $b contiene el segundo elemento.
-    echo "A: $a \n";
-}
-?>
